3acb35fe0797254b586d9642bef752c0a11dac70
Anna Jamroz <jamroz.anna95@gmail.com>
2023-04-01 12:22:22 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+tns({
+    container: '.slider__element',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    navPosition: "bottom",
+    mouseDrag: true,
+});
+
+let navigation = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 15) {
+        navigation.classList.add('navbar--move')
+    } else {
+        navigation.classList.remove('navbar--move')
+    }
+})
+
+let hemburger = document.querySelector(".navbar__hamburger")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("navbar__hamburger--active")
+    navigation.classList.toggle("navbar--open")
+})
